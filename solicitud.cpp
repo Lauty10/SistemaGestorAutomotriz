51880ceb0055934b53e5c43c5d19548a4dbb907c
Lautaro Maldonado <lautylbs@hotmail.com>
2024-12-11 04:09:59 -0300

    ss

--- solicitud.cpp
+++ solicitud.cpp
@@ -51,7 +51,17 @@ int Solicitud::getId(){
 return id;
 }
 
-
+bool Solicitud::abrir_crear_arch_solicitud(){
+FILE *ptr_a_arch_solicitudes;
+ptr_a_arch_solicitudes=fopen("Solicitud.dat","ab");
+if(ptr_a_arch_solicitudes==NULL){
+    rlutil::locate(45,18);
+    cout<<"Error al abrir o crear archivo"<<endl;
+    return false;
+}
+fclose(ptr_a_arch_solicitudes);
+return true;
+}
 void Solicitud::cargarSolicitud(){
     int nuevaSoli;
     bool control=true;
@@ -65,7 +75,7 @@ void Solicitud::cargarSolicitud(){
      rlutil::locate(15,11);
      cout << "-> Ingrese su nombre para su alta:";
      rlutil::locate(50,11);
-     cin.getline(nombre, 30);
+     cin.getline(nombre, 30,'\n');
      if(validarNombre(nombre)){control=false;}
      else{
           rlutil::setColor(rlutil::LIGHTRED);
@@ -77,23 +87,309 @@ void Solicitud::cargarSolicitud(){
           cout << "Intentelo de nuevo. ";
           rlutil::setColor(rlutil::WHITE);}
     }while(control);
-    setNombre(nombre);
+    //setNombre(nombre);
     rlutil::locate(50,11);
     cout << "                               ";
     rlutil::locate(15,14);
     cout << "                                                                                              ";
     rlutil::locate(15,16);
     cout << "                     ";
+    control=true;
+    do{
     rlutil::locate(15,11);
-    cout << "-> Ingrese su correo para su alta:";
+    cout << "-> Ingrese su correo electronico para su alta: ";
+    rlutil::locate(62,11);
     cin.getline(correo, 35, '\n');
-    setCorreo(correo);
+    switch(validarC(correo)){
+     case 0:
+     control=false;
+     break;
+     case 1:
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,18);
+          cout << "                    ";
+          rlutil::locate(15,19);
+          cout << "                    ";
+          rlutil::locate(15,20);
+          cout << "                    ";
+          rlutil::locate(15,21);
+          cout << "                    ";
+          rlutil::locate(15,23);
+          cout << "                    ";
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El correo ingresado es demasiado corto";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 2:
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,18);
+          cout << "                    ";
+          rlutil::locate(15,19);
+          cout << "                    ";
+          rlutil::locate(15,20);
+          cout << "                    ";
+          rlutil::locate(15,21);
+          cout << "                    ";
+          rlutil::locate(15,23);
+          cout << "                    ";
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El correo ingresado debe contener (@) ";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 3:
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,18);
+          cout << "                    ";
+          rlutil::locate(15,19);
+          cout << "                    ";
+          rlutil::locate(15,20);
+          cout << "                    ";
+          rlutil::locate(15,21);
+          cout << "                    ";
+          rlutil::locate(15,23);
+          cout << "                    ";
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El correo ingresado no debe contener mas de 1 (@).";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 4:
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El dominio de este correo no esta permitido en nuestro sistema";
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,16);
+          cout << "Dominios permitidos:";
+          rlutil::locate(15,18);
+          cout << "> @gmail.com";
+          rlutil::locate(15,19);
+          cout << "> @hotmail.com";
+          rlutil::locate(15,20);
+          cout << "> @outlook.com";
+          rlutil::locate(15,21);
+          cout << "> @yahoo.com";
+          rlutil::locate(15,23);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 5:
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,18);
+          cout << "                    ";
+          rlutil::locate(15,19);
+          cout << "                    ";
+          rlutil::locate(15,20);
+          cout << "                    ";
+          rlutil::locate(15,21);
+          cout << "                    ";
+          rlutil::locate(15,23);
+          cout << "                    ";
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El correo ingresado no debe contener mas caracteres despues del dominio permitido";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 6:
+          rlutil::locate(15,16);
+          cout << "                    ";
+          rlutil::locate(15,18);
+          cout << "                    ";
+          rlutil::locate(15,19);
+          cout << "                    ";
+          rlutil::locate(15,20);
+          cout << "                    ";
+          rlutil::locate(15,21);
+          cout << "                    ";
+          rlutil::locate(15,23);
+          cout << "                    ";
+          rlutil::locate(47,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "                                                                      ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                                      ";
+          rlutil::locate(15,14);
+          cout << "Error: El correo ingresado ya se encuentra registrado";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+
+     default:
+      cout<<"Error desconocido";
+        break;
+
+    }
+    }while (control);
+    rlutil::setColor(rlutil::WHITE);
+    //setCorreo(correo);
+    rlutil::locate(15,16);
+    cout << "                    ";
+    rlutil::locate(15,18);
+    cout << "                    ";
+    rlutil::locate(15,19);
+    cout << "                    ";
+    rlutil::locate(15,20);
+    cout << "                    ";
+    rlutil::locate(15,21);
+    cout << "                    ";
+    rlutil::locate(15,23);
+    cout << "                    ";
+    rlutil::locate(47,11);
+    cout << "                                                                      ";
+    rlutil::locate(15,14);
+    cout << "                                                                      ";
+    rlutil::locate(50,11);
+    cout << "                                                                      ";
+    rlutil::locate(15,16);
+    cout << "                                                                      ";
+    rlutil::locate(50,11);
+    cout << "                                                                      ";
+    control=true;
+    do{
+    rlutil::locate(15,11);
+    cout << "-> Ingrese su dni para su alta: ";
+    rlutil::locate(47,11);
+    cin.getline(dni,10);
+    switch(validarD(dni)){
+     case 0:
+     control=false;
+     break;
+     case 1:
+          rlutil::locate(47,11);
+          cout << "                                                             ";
+          rlutil::locate(15,14);
+          cout << "                                                              ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                               ";
+          rlutil::locate(15,14);
+          cout << "Error: El DNI no puede ser negativo.";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 2:
+          rlutil::locate(47,11);
+          cout << "                                                           ";
+          rlutil::locate(15,14);
+          cout << "                                                           ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                                                           ";
+          rlutil::locate(15,14);
+          cout << "Error: El DNI no debe contener letras o simbolos especiales.";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 3:
+          rlutil::locate(47,11);
+          cout << "                                                           ";
+          rlutil::locate(15,14);
+          cout << "                                                            ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                               ";
+          rlutil::locate(15,14);
+          cout << "Error: El DNI debe contener 7 u 8 digitos.";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 4:
+          rlutil::locate(47,11);
+          cout << "                                                            ";
+          rlutil::locate(15,14);
+          cout << "                                                            ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                               ";
+          rlutil::locate(15,14);
+          cout << "Error: El DNI no debe empezar en 0";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+     case 5:
+          rlutil::locate(47,11);
+          cout << "                                                            ";
+          rlutil::locate(15,14);
+          cout << "                                                            ";
+          rlutil::setColor(rlutil::LIGHTRED);
+          rlutil::locate(50,11);
+          cout << "                               ";
+          rlutil::locate(15,14);
+          cout << "Error: Este DNI ya se encuentra registrado ";
+          rlutil::locate(15,16);
+          cout << "Intentelo de nuevo. ";
+          rlutil::setColor(rlutil::WHITE);
+     break;
+
+
+     default:
+      cout<<"Error desconocido";
+        break;
+
+    }
+
+    }while (control);
+
+    rlutil::locate(15,11);
+    cout << "                                                                                              ";
+    rlutil::locate(50,11);
+    cout << "                               ";
+    rlutil::locate(15,14);
+    cout << "                                                                                              ";
+    rlutil::locate(15,16);
+    cout << "                     ";
     rlutil::locate(45,14);
-    cout << "Ingrese su dni para su alta:";
-    cin >> dni;
-    cin.ignore();
-    setDni(dni);
-    rlutil::locate(45,16);
     cout << "El numero de solicitud es: " << getId() << endl;
     rlutil::locate(45,18);
     system("pause");
@@ -230,29 +526,52 @@ GraficarLineasHorizontales(1,121,28,false,205);
 
 
 //VALIDACIONES
-//Nombre
-void Solicitud::validarC(char* c){
+
+int Solicitud::validarC(char* c){
+int longitud=strlen(c);
+if(longitud<11){
+return 1;
+}
+int arroba=0;
+for(int x=0;x<longitud;x++){
+if(c[x]=='@'){
+arroba++;
+}
+}
+if(arroba==0){
+    return 2;
+}else if(arroba>=2){
+return 3;
+}
+char*dominio=strstr(c,"@");
+dominio++;
+if(strcmp(dominio,"gmail.com")!=0
+             &&
+   strcmp(dominio,"hotmail.com")!=0
+             &&
+   strcmp(dominio,"outlook.com")!=0
+             &&
+   strcmp(dominio,"yahoo.com")!=0
+){return 4;}
+
+char *fin_dominio=dominio+strlen(dominio);
+if(*fin_dominio!='\0'){
+    return 5;
+}
 FILE *correo;
 correo=fopen("Solicitud.dat","rb");
 if(correo==NULL){
 cout<<"NO SE PUDO ABRIR ESTE ARCHIVO"<<endl;
-return;
+return -1;
 }
 Solicitud obj;
-while(c[0]==0){
-cout<<"Campo incompleto"<<endl;
-cin.ignore();
-cin.getline(c,30,'\n');
-}
 while(fread(&obj,sizeof(Solicitud),1,correo)!=0){
 if(strcmp(obj.getCorreo(),c)==0){
-cout<<"Este correo fue dado de alta, igrese uno distinto"<<endl;
-cout<<"Correo: ";
-cin.ignore();
-cin.getline(c,30,'\n');
+return 6;
 }
 }
 fclose(correo);
+return 0;
 }
 
 //NOMBRE
@@ -266,22 +585,44 @@ return true;
 }
 
 //DNI
-bool Solicitud::validarD(char* d){
+//1 negativo, 2 letra,3,limiite,4 cero, 0 valido
+
+int Solicitud::validarD(char* d){
 int digitos=0;
 for(int y=0;d[y]!='\0';y++){
-    if(!isdigit(d[y])){
-        return false;
+    if(d[y]=='-'){
+        return 1;
+    }else if(!isdigit(d[y])){
+        return 2;
     }
-    digitos++;
-}
-if(digitos<7||digitos>8){
-    return false;
+     digitos++;
 }
 
+if(digitos>8){
+    return 3;
+}
+
+if(digitos<7){
+    return 3;
+}
 if(d[0]=='0'){
-    return true;
+    return 4;
 
 }
+FILE *ptr_a_arch_solicitudes;
+ptr_a_arch_solicitudes=fopen("Solicitud.dat","rb");
+if(ptr_a_arch_solicitudes==NULL){
+cout<<"NO SE PUDO ABRIR ESTE ARCHIVO"<<endl;
+return -1;
+}
+Solicitud obj_;
+while(fread(&obj_,sizeof(Solicitud),1,ptr_a_arch_solicitudes)!=0){
+if(strcmp(obj_.getDni(),dni)==0){
+return 5;
+}
+}
+fclose(ptr_a_arch_solicitudes);
+return 0;
 }
 
 void Solicitud::estiloV(){
